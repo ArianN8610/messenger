0185122788a339c1a69b4417a7adb0597622a130
Arian <ariannasiri86@gmail.com>
2024-11-20 21:18:17 +0330

    Create user model

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password', )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                        'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ['email', 'username', 'is_active', 'is_staff', 'is_superuser']
+    search_fields = ('email', 'username')
+    ordering = ('email', 'username')
--- core/core/settings.py
+++ core/core/settings.py
@@ -133,3 +133,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# User manager config
+
+AUTH_USER_MODEL = "account.User"
+LOGIN_REDIRECT_URL = "/"
