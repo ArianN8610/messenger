e6102db10cd9953213a4171b0bd0947f3d8a42cb
Arian <ariannasiri86@gmail.com>
2025-02-01 23:54:36 +0330

    Create PrivateChat and Message model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -24,5 +24,8 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(max_length=100, blank=True)
 
-    def __str__(self):
+    def get_full_name(self):
         return f'{self.first_name} {self.last_name}'.strip()
+
+    def __str__(self):
+        return self.get_full_name()
--- core/messenger/models.py
+++ core/messenger/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class PrivateChat(models.Model):
+    user1 = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, related_name='chats_initiated', null=True)
+    user2 = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, related_name='chats_received', null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user1', 'user2')  # Both users only have one private chat
+
+    def __str__(self):
+        return f'{self.user1.username} & {self.user2.username}'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey('accounts.User', related_name='messages', on_delete=models.SET_NULL, null=True)
+    chat = models.ForeignKey(PrivateChat, on_delete=models.CASCADE, related_name='messages')
+
+    content = models.TextField()
+    reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+
+    seen_at = models.DateTimeField(blank=True, null=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.sender.username} -> ({self.chat.__str__()})'
