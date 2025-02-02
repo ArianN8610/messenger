1f026c1804a01d63b3680729d6f88793e13bae5c
Arian <ariannasiri86@gmail.com>
2025-02-02 19:01:09 +0330

    Create admin.py for messenger

--- core/messenger/admin.py
+++ core/messenger/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import PrivateChat, Message
 
-# Register your models here.
+
+@admin.register(PrivateChat)
+class PrivateChatAdmin(admin.ModelAdmin):
+    list_display = ('user1', 'user2')
+    search_fields = ('user1__email', 'user1__username', 'user2__email', 'user2__username')
+    ordering = ('-created_at',)
+    autocomplete_fields = ('user1', 'user2')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'chat', 'seen_at', 'sent_at')
+    search_fields = ('sender__email', 'sender__username')
+    ordering = ('-sent_at',)
+    autocomplete_fields = ('sender', 'chat')
